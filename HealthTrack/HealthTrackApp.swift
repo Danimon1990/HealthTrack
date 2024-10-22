8f93e25a4b979998fd626caf7176200cb736e4c7
Daniel Moreno <bastian.moreton@gmail.com>
2024-10-22 07:21:40 -0500

    Initial Commit

---
+++ HealthTrack/HealthTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackApp.swift
+//  HealthTrack
+//
+//  Created by Daniel Moreno on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthTrackTests/HealthTrackTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthTrackTests.swift
+//  HealthTrackTests
+//
+//  Created by Daniel Moreno on 10/22/24.
+//
+
+import Testing
+
+struct HealthTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
